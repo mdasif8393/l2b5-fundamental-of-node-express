1e5cb7833beb65f1e2181707eb07814d843b3463
mdasif8393 <md.asif.8393@gmail.com>
2025-05-31 20:14:44 +0600

    13-7 Routing in node js

--- module-2/todo/server.js
+++ module-2/todo/server.js
@@ -1,7 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.end("Welcome to to do server")
+    if (req.url === "/todos" && req.method === "GET") {
+        res.end("All ToDos")
+    }
+    else if (req.url === "/todos/create-todo" && req.method === "POST") {
+        res.end("ToDo Created")
+    }
+    else {
+        res.end("Route not found")
+    }
+
 })
 
 server.listen(5000, "127.0.0.1", () => {
